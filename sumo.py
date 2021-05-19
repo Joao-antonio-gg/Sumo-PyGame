85dc37adbd3837887b9a8d3e8c38411526f6a1d9
joao <joaoagg@al.insper.edu.br>
2021-05-19 20:05:41 -0300

    comida aparecendo Co-authored-by: Enzo Del Manto <EnzoDelManto@users.noreply.github.com> Co-authored-by: Arthur Motta <thurra97@users.noreply.github.com>

--- sumo.py
+++ sumo.py
@@ -16,42 +16,42 @@ janela = pygame.display.set_mode((largura,altura))
 pygame.display.set_caption("Sumo Eats")
 
 #Importar imagens
-sumo = pygame.image.load('sumo_parado.png')
-bigorna = pygame.image.load('bigorna.png')
-onigiri = pygame.image.load('onigiri.png')
-sushi = pygame.image.load('sushi.png')
-lamen = pygame.image.load('lamen.png')
-doce = pygame.image.load('doce.png')
+sumopng = pygame.image.load('Assets/sumo_parado.png')
+bigornapng = pygame.image.load('Assets/bigorna.png')
+onigiripng = pygame.image.load('Assets/onigiri.png')
+sushipng = pygame.image.load('Assets/sushi.png')
+lamenpng = pygame.image.load('Assets/lamen.png')
+docepng = pygame.image.load('Assets/doce.png')
 
-comida_largura = 80
-comida_altura = 80
-sumo_largura = 200
-sumo_altura = 200
-bigorna_altura = 90
-bigorna_largura = 90
+comida_largura = 60
+comida_altura = 60
+sumo_largura = 300
+sumo_altura = 300
+bigorna_altura = 80
+bigorna_largura = 100
 
 assets = {}
 #Importar animações
-direita = [pygame.image.load('d0.png'), pygame.image.load('d1.png'), pygame.image.load('d2.png')]
-esquerda = [pygame.image.load('e0.png'), pygame.image.load('e1.png'), pygame.image.load('e2.png')]
-comer = [pygame.image.load('c0.png'), pygame.image.load('c1.png'), pygame.image.load('c2.png'), pygame.image.load('c3.png'), pygame.image.load('c4.png')]
-background = [pygame.image.load('frame-001.png'), pygame.image.load('frame-002.png'), pygame.image.load('frame-003.png'), pygame.image.load('frame-004.png'), pygame.image.load('frame-005.png'), pygame.image.load('frame-006.png'), pygame.image.load('frame-007.png'), pygame.image.load('frame-008.png'), pygame.image.load('frame-009.png'), pygame.image.load('frame-010.png'), pygame.image.load('frame-011.png'), pygame.image.load('frame-012.png'), pygame.image.load('frame-013.png'), pygame.image.load('frame-014.png'), pygame.image.load('frame-015.png'), pygame.image.load('frame-016.png'), pygame.image.load('frame-017.png'), pygame.image.load('frame-018.png'), pygame.image.load('frame-019.png'), pygame.image.load('frame-020.png'), pygame.image.load('frame-021.png'), pygame.image.load('frame-022.png'), pygame.image.load('frame-023.png'), pygame.image.load('frame-024.png'), pygame.image.load('frame-025.png'), pygame.image.load('frame-026.png'), pygame.image.load('frame-027.png'), pygame.image.load('frame-028.png'), pygame.image.load('frame-029.png'), pygame.image.load('frame-030.png'), pygame.image.load('frame-031.png'), pygame.image.load('frame-032.png'), pygame.image.load('frame-033.png'), pygame.image.load('frame-034.png'), pygame.image.load('frame-035.png'), pygame.image.load('frame-036.png'), pygame.image.load('frame-037.png'), pygame.image.load('frame-038.png'), pygame.image.load('frame-039.png'), pygame.image.load('frame-040.png'), pygame.image.load('frame-041.png'), pygame.image.load('frame-042.png'), pygame.image.load('frame-043.png'), pygame.image.load('frame-044.png'), pygame.image.load('frame-045.png'), pygame.image.load('frame-046.png'), pygame.image.load('frame-047.png'), pygame.image.load('frame-048.png'), pygame.image.load('frame-049.png')]
+direita = [pygame.image.load('Assets/d0.png'), pygame.image.load('Assets/d1.png'), pygame.image.load('Assets/d2.png')]
+esquerda = [pygame.image.load('Assets/e0.png'), pygame.image.load('Assets/e1.png'), pygame.image.load('Assets/e2.png')]
+comer = [pygame.image.load('Assets/c0.png'), pygame.image.load('Assets/c1.png'), pygame.image.load('Assets/c2.png'), pygame.image.load('Assets/c3.png'), pygame.image.load('Assets/c4.png')]
+background = [pygame.image.load('Assets/frame-001.png'), pygame.image.load('Assets/frame-002.png'), pygame.image.load('Assets/frame-003.png'), pygame.image.load('Assets/frame-004.png'), pygame.image.load('Assets/frame-005.png'), pygame.image.load('Assets/frame-006.png'), pygame.image.load('Assets/frame-007.png'), pygame.image.load('Assets/frame-008.png'), pygame.image.load('Assets/frame-009.png'), pygame.image.load('Assets/frame-010.png'), pygame.image.load('Assets/frame-011.png'), pygame.image.load('Assets/frame-012.png'), pygame.image.load('Assets/frame-013.png'), pygame.image.load('Assets/frame-014.png'), pygame.image.load('Assets/frame-015.png'), pygame.image.load('Assets/frame-016.png'), pygame.image.load('Assets/frame-017.png'), pygame.image.load('Assets/frame-018.png'), pygame.image.load('Assets/frame-019.png'), pygame.image.load('Assets/frame-020.png'), pygame.image.load('Assets/frame-021.png'), pygame.image.load('Assets/frame-022.png'), pygame.image.load('Assets/frame-023.png'), pygame.image.load('Assets/frame-024.png'), pygame.image.load('Assets/frame-025.png'), pygame.image.load('Assets/frame-026.png'), pygame.image.load('Assets/frame-027.png'), pygame.image.load('Assets/frame-028.png'), pygame.image.load('Assets/frame-029.png'), pygame.image.load('Assets/frame-030.png'), pygame.image.load('Assets/frame-031.png'), pygame.image.load('Assets/frame-032.png'), pygame.image.load('Assets/frame-033.png'), pygame.image.load('Assets/frame-034.png'), pygame.image.load('Assets/frame-035.png'), pygame.image.load('Assets/frame-036.png'), pygame.image.load('Assets/frame-037.png'), pygame.image.load('Assets/frame-038.png'), pygame.image.load('Assets/frame-039.png'), pygame.image.load('Assets/frame-040.png'), pygame.image.load('Assets/frame-041.png'), pygame.image.load('Assets/frame-042.png'), pygame.image.load('Assets/frame-043.png'), pygame.image.load('Assets/frame-044.png'), pygame.image.load('Assets/frame-045.png'), pygame.image.load('Assets/frame-046.png'), pygame.image.load('Assets/frame-047.png'), pygame.image.load('Assets/frame-048.png'), pygame.image.load('Assets/frame-049.png')]
 
 #Importar sons
-som_comer = pygame.mixer.Sound('comer.mp3')
-som_pontos = pygame.mixer.Sound('checkpoint.mp3')
-som_perdeu = pygame.mixer.Sound('lose.mp3')
-musica = pygame.mixer.music.load('musica.mp3')
+som_comer = pygame.mixer.Sound('Assets/comer.mp3')
+som_pontos = pygame.mixer.Sound('Assets/checkpoint.mp3')
+som_perdeu = pygame.mixer.Sound('Assets/lose.mp3')
+musica = pygame.mixer.music.load('Assets/musica.mp3')
 pygame.mixer.music.play(-1)
 
-assets["fonte_pontos"] = pygame.font.Font('font/fonte_sumo.ttf', 28)
+assets["fonte_pontos"] = pygame.font.Font('Assets/fonte_sumo.ttf', 28)
 
-onigiripng = pygame.transform.scale(onigiri, (comida_largura, comida_altura))
-lamenpng = pygame.transform.scale(lamen, (comida_largura, comida_altura))
-docepng = pygame.transform.scale(doce, (comida_largura, comida_altura))
-sushipng = pygame.transform.scale(sushi, (comida_largura, comida_altura))
-sumopng = pygame.transform.scale(sumo, (sumo_largura, sumo_altura))
-bigornapng = pygame.transform.scale(bigorna, (bigorna_largura, bigorna_altura))
+onigiripng = pygame.transform.scale(onigiripng, (comida_largura, comida_altura))
+lamenpng = pygame.transform.scale(lamenpng, (comida_largura, comida_altura))
+docepng = pygame.transform.scale(docepng, (comida_largura, comida_altura))
+sushipng = pygame.transform.scale(sushipng, (comida_largura, comida_altura))
+sumopng = pygame.transform.scale(sumopng, (sumo_largura, sumo_altura))
+bigornapng = pygame.transform.scale(bigornapng, (bigorna_largura, bigorna_altura))
 
 class sumo(pygame.sprite.Sprite):
     def __init__(self, img):
@@ -64,15 +64,15 @@ class sumo(pygame.sprite.Sprite):
         self.speedx = 0
         self.mask = pygame.mask.from_surface(sumopng)
 
-def update(self):
+    def update(self):
     # Atualização da posição do boneco
-    self.rect.x += self.speedx
+        self.rect.x += self.speedx
  
     # Mantem dentro da tela
-    if self.rect.right > largura:
-        self.rect.right = largura
-    if self.rect.left < 0:
-        self.rect.left = 0
+        if self.rect.right > largura:
+            self.rect.right = largura
+        if self.rect.left < 0:
+            self.rect.left = 0
 
 class comidas(pygame.sprite.Sprite):
     def __init__(self, img):
@@ -88,6 +88,15 @@ class comidas(pygame.sprite.Sprite):
         self.mask = pygame.mask.from_surface(sushipng)
         self.mask = pygame.mask.from_surface(lamenpng)
         self.mask = pygame.mask.from_surface(docepng)
+    def update(self):
+        # Atualizando a posição da fruta 
+        self.rect.y += self.speedy
+        # Se a fruta passar do final da tela, volta para cima e sorteia
+        # novas posições e velocidades
+        if self.rect.top > altura or self.rect.right < 0 or self.rect.left > largura:
+            self.rect.x = random.randint(0, largura-comida_largura)
+            self.rect.y = random.randint(-100, -comida_altura)
+            self.speedy = random.randint(2, 5)
         
 class bigorna(pygame.sprite.Sprite):
     def __init__(self, img):
@@ -101,10 +110,9 @@ class bigorna(pygame.sprite.Sprite):
         self.speedy = random.randint(2, 5)
         self.mask = pygame.mask.from_surface(bigornapng)
     def update(self):
-        # Atualizando a posição da bomba  
+        # Atualizando a posição da bigorna  
         self.rect.y += self.speedy
-        # Se a bomba passar do final da tela, volta para cima e sorteia
-        # novas posições e velocidades
+        # Se a bigorna passar do final da tela, volta para cima e sorteia novas posições e velocidades
         if self.rect.top > altura or self.rect.right < 0 or self.rect.left > largura:
             self.rect.x = random.randint(0, largura - comida_largura)
             self.rect.y = random.randint(-100, - comida_largura)
@@ -122,30 +130,31 @@ sushi_sprite = pygame.sprite.Group()
 doce_sprite = pygame.sprite.Group()
 lamen_sprite = pygame.sprite.Group()
 bigorna_sprite = pygame.sprite.Group()
+sumo_sprite = sumo(sumopng)
 
 #cria sushi
-for i in range(3):
-    sushi = comidas(sushipng)
-    sprites.add(sushi)
-    sushi_sprite.add(sushi)
+for i in range(1):
+    sushidif = comidas(sushipng)
+    sprites.add(sushidif)
+    sushi_sprite.add(sushidif)
 
 #cria onigiri
-for i in range(2):
-    onigiri = comidas(onigiripng)
-    sprites.add(onigiri)
-    onigiri_sprite.add(onigiri)
+for i in range(1):
+    onigiridif = comidas(onigiripng)
+    sprites.add(onigiridif)
+    onigiri_sprite.add(onigiridif)
     
 #cria lamen
-for i in range(4):
-    lamen = comidas(lamenpng)
-    sprites.add(lamen)
-    lamen_sprite.add(lamen)
+for i in range(1):
+    lamendif = comidas(lamenpng)
+    sprites.add(lamendif)
+    lamen_sprite.add(lamendif)
     
 #cria doce
 for i in range(1):
-    doce = comidas(docepng)
-    sprites.add(doce)
-    doce_sprite.add(doce)
+    docedif = comidas(docepng)
+    sprites.add(docedif)
+    doce_sprite.add(docedif)
 
 #---------------------------------------------------------------------------------------------------------------
 ##ESPACO PONTUACAO TELA INICIAL
@@ -159,7 +168,7 @@ end_it=False
 
 #cria tela de início
 while (end_it==False):
-    startscreen=pygame.image.load(background)
+    startscreen=pygame.image.load('Assets/bigorna.png')
     janela.blit(startscreen, (0,0))
     myfont=pygame.font.SysFont("Britannic Bold", 40)
     nlabel=myfont.render("Press ENTER to Start", 1, (255, 255, 255))
@@ -173,7 +182,7 @@ while (end_it==False):
     janela.blit(nlabel,(200,200))
     pygame.display.flip()
 #---------------------------------------------------------------------------------------------------------------
-
+game_on = True
 while game_on:
 #aumenta o número de bigornas a cada 100 pontos
     if len(bigorna_sprite.sprites()) < (score//100 + 1):
@@ -191,30 +200,30 @@ while game_on:
         if event.type == pygame.KEYDOWN:
             # Dependendo da tecla, altera a velocidade.
             if event.key == pygame.K_LEFT:
-                sumo.speedx -= 8
+                sumo_sprite.speedx -= 8
             if event.key == pygame.K_RIGHT:
-                sumo.speedx += 8
+                sumo_sprite.speedx += 8
             # Verifica se soltou alguma tecla.
         if event.type == pygame.KEYUP:
             # Dependendo da tecla, altera a velocidade.
             if event.key == pygame.K_LEFT:
-                sumo.speedx += 8
+                sumo_sprite.speedx += 8
             if event.key == pygame.K_RIGHT:
-                sumo.speedx -= 8
+                sumo_sprite.speedx -= 8
 
     sprites.update()
 
     # Verifica se houve contato entre o player e a bomba
-    hits = pygame.sprite.spritecollide(sumo, bigorna, True, pygame.sprite.collide_mask)
+    hits = pygame.sprite.spritecollide(sumo_sprite, bigorna_sprite, True, pygame.sprite.collide_mask)
     for bigorna_sprite in hits:     
         som_perdeu.play()
         time.sleep(1)
         game_on = False
 
-    hits2 = pygame.sprite.spritecollide(sumo, sushi_sprite, True, pygame.sprite.collide_mask)
-    hits3 = pygame.sprite.spritecollide(sumo, onigiri_sprite, True, pygame.sprite.collide_mask) 
-    hits4 = pygame.sprite.spritecollide(sumo, lamen_sprite, True, pygame.sprite.collide_mask)
-    hits5 = pygame.sprite.spritecollide(sumo, doce_sprite, True, pygame.sprite.collide_mask) 
+    hits2 = pygame.sprite.spritecollide(sumo_sprite, sushi_sprite, True, pygame.sprite.collide_mask)
+    hits3 = pygame.sprite.spritecollide(sumo_sprite, onigiri_sprite, True, pygame.sprite.collide_mask) 
+    hits4 = pygame.sprite.spritecollide(sumo_sprite, lamen_sprite, True, pygame.sprite.collide_mask)
+    hits5 = pygame.sprite.spritecollide(sumo_sprite, doce_sprite, True, pygame.sprite.collide_mask) 
 
     #gera um novo sushi para cada hit
     for sushi in hits2:
@@ -249,9 +258,8 @@ while game_on:
         score += 20
 
     janela.fill((0,0,0))
-    janela.blit(background, [0,0])
     #adiciona o score na tela
-    text_surface = assets['fonte_pontos'].render("{:08d}".format(score), True, (255, 255, 0))
+    text_surface = assets['fonte_pontos'].render("{:01d}".format(score), True, (255, 255, 0))
     text_rect = text_surface.get_rect()
     text_rect.midtop = (largura / 2,  10)
     janela.blit(text_surface, text_rect)
@@ -268,10 +276,10 @@ listascore.append(score)
 if game_on == False:
     janela.fill((0, 0, 0))
     myfont=pygame.font.SysFont("Britannic Bold", 50)
-    nlabel=myfont.render("Você fez {:08d} pontos".format(score), 1, (255, 255, 255))
+    nlabel=myfont.render("Você fez {:01d} pontos".format(score), 1, (255, 255, 255))
     nlabel2=myfont.render("Game Over", 1, (255, 0, 0))
-    janela.blit(nlabel,(100,200))
-    janela.blit(nlabel2,(200,150))
+    janela.blit(nlabel,(400,300))
+    janela.blit(nlabel2,(450,250))
     pygame.display.flip()
     time.sleep(3)
 
